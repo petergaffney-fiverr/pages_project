3d2ab4fbe59fc08771876fbaa5ce288e9d0b564a
A26-Lab <a26.lab.syl@gmail.com>
2020-08-03 06:05:09 +0600

    update

--- pages_project/settings.py
+++ pages_project/settings.py
@@ -114,7 +114,14 @@ USE_L10N = True
 
 USE_TZ = True
 
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT=os.path.join(BASE_DIR, "media")
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
